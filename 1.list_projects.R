44b09137cf99372a91ec9f4b1a39a54f8cf260d9
Jean-Olivier Irisson <irisson@normalesup.org>
2020-12-15 16:50:58 +0000

    List candidate projects

---
+++ 1.list_projects.R
@@ -0,0 +1,23 @@
+#
+# List EcoPart projects and determine which to use
+#
+# (c) 2020 Jean-Olivier Irisson, GNU General Public License v3
+
+library("tidyverse")
+library("ecotaxar")
+
+db <- db_connect_ecotaxa()
+
+# get ids and titles from the database
+projects <- tbl(db, "part_projects") %>%
+  select(pprojid, ptitle, data_owner=do_email, projid) %>%
+  # add EcoTaxa title
+  left_join(tbl(db, "projects") %>% select(projid, title)) %>%
+  collect() %>%
+  arrange(title) %>%
+  # add empty column
+  mutate(use="")
+
+# write it into a file
+write_tsv(projects, "data/UVP5_projects.tsv")
+# now select which ones can be used in the global dataset
