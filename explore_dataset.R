1f48b3f9c98425ae52b6c90673938cf987a1e845
Jean-Olivier Irisson <irisson@normalesup.org>
2020-12-15 17:51:47 +0000

    Add a map at sample level

---
+++ explore_dataset.R
@@ -0,0 +1,16 @@
+#
+# Explore data set properties and extract nice figures
+#
+# (c) 2020 Jean-Olivier Irisson, GNU General Public License v3
+
+library("tidyverse")
+library("chroma")
+dir.create("figures", showWarnings=FALSE)
+
+s <- read_tsv("data/UVP5_selected_samples.tsv")
+coast <- read_csv("data/gshhg_world_c.csv.gz")
+
+ggplot() + coord_quickmap() + scale_xy_map() +
+  geom_polygon(aes(lon, lat), data=coast, fill="grey60") +
+  geom_point(aes(lon, lat), data=s, size=0.1)
+ggsave("figures/map.pdf", width=6, heigh=3.5)
