22c62cce98ba96e15ac84dc180a2df37040814cd
Jean-Olivier Irisson <irisson@normalesup.org>
2020-12-16 00:06:19 +0000

    Extract volumes from database

---
+++ 4.get_volumes.R
@@ -0,0 +1,19 @@
+#
+# Extract sampled volume to be able to compute concentrations
+#
+# (c) 2020 Jean-Olivier Irisson, GNU General Public License v3
+
+source("0.setup.R")
+
+# read selected samples
+samples <- read_tsv("data/UVP5_samples.tsv", col_types=cols()) %>%
+  select(sampleid, psampleid)
+
+volumes <- samples %>%
+  left_join(tbl(db, "part_histopart_reduit") %>% select(psampleid, mid_depth_bin=depth, water_volume_imaged=watervolume), copy=TRUE) %>%
+  select(-psampleid) %>%
+  collect()
+
+summary(volumes)
+
+write_tsv(volumes, "data/UVP5_volumes.tsv.gz")
