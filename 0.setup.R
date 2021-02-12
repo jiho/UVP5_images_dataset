df0ca72332c3a9923d18d1e712b62ab1d5c36262
Jean-Olivier Irisson <irisson@normalesup.org>
2021-02-12 22:49:14 +0000

    New path to large files

--- 0.setup.R
+++ 0.setup.R
@@ -9,9 +9,9 @@ library("ecotaxar")
 db <- db_connect_ecotaxa()
 
 # create directories for large data that should live outside the repository
-data_dir <- "~/UVP5_images_dataset/"
-proj_dir <- "~/UVP5_images_dataset/projects/"
-img_dir <- "~/UVP5_images_dataset/images/"
+data_dir <- "~/datasets/UVP5_images_dataset/"
+proj_dir <- "~/datasets/UVP5_images_dataset/projects/"
+img_dir <- "~/datasets/UVP5_images_dataset/images/"
 
 dir.create(data_dir, showWarnings=FALSE, recursive=TRUE)
 dir.create(proj_dir, showWarnings=FALSE, recursive=TRUE)
