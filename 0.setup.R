8e3da4b3e445c1dea7aa4f5c595085f0450cea87
Jean-Olivier Irisson <irisson@normalesup.org>
2021-02-19 11:03:44 +0000

    Switch away from the fs package which was crashing

--- 0.setup.R
+++ 0.setup.R
@@ -9,9 +9,10 @@ library("ecotaxar")
 db <- db_connect_ecotaxa()
 
 # create directories for large data that should live outside the repository
-data_dir <- "~/datasets/UVP5_images_dataset/"
-proj_dir <- "~/datasets/UVP5_images_dataset/projects/"
-img_dir <- "~/datasets/UVP5_images_dataset/images/"
+# NB: no trailing /
+data_dir <- "~/datasets/UVP5_images_dataset"
+proj_dir <- "~/datasets/UVP5_images_dataset/projects"
+img_dir <- "~/datasets/UVP5_images_dataset/images"
 
 dir.create(data_dir, showWarnings=FALSE, recursive=TRUE)
 dir.create(proj_dir, showWarnings=FALSE, recursive=TRUE)
--- 7.get_images.R
+++ 7.get_images.R
@@ -5,42 +5,47 @@
 
 source("0.setup.R")
 library("feather")
-library("fs")
 library("furrr")
 
 # read relevant pieces of the data table
-o <- read_feather(path(data_dir, "all.feather"), columns=c("projid", "sampleid", "objid", "file_name"))
+o <- read_feather(file.path(data_dir, "all.feather"), columns=c("projid", "sampleid", "objid", "file_name"))
 
 
 ## Copy all files to the images directory ----
 
 # homogenize the path to img_dir (transform ~ into /home/.../ )
-img_dir <- path_expand(img_dir)
+img_dir <- path.expand(img_dir)
 
 # make sub directories per sample, to avoid having too many files in a single directory
-dir_create(img_dir, unique(o$sampleid))
+walk(file.path(img_dir, unique(o$sampleid)), dir.create, showWarnings=FALSE)
 
 # prepare the source and destination for each image
 o <- mutate(o,
-  source=path("/remote/ecotaxa/vault/", file_name),
-  dest=path(img_dir, sampleid, objid, ext="jpg")
+  source=file.path("/remote/ecotaxa/vault", file_name),
+  dest  =file.path(img_dir, sampleid, str_c(objid, ".jpg"))
 )
 
 # list images already copied
-copied <- dir_ls(img_dir, recurse=TRUE, type="file")
+copied <- system2("find", args=str_c(img_dir, " -type f"), stdout=TRUE)
 length(copied)
 # and remove them from the list
-o <- filter(o, ! dest %in% copied)
+o_to_copy <- filter(o, ! dest %in% copied)
 
 # copy the remaining images, in parallel
 plan(multisession, workers=5)
-o %>% split(o$sampleid) %>% future_walk(~file_copy(.$source, .$dest))
+o_to_copy %>% split(o$sampleid) %>% future_walk(~file.copy(.$source, .$dest))
 # close the parallel workers
 plan(sequential)
 
 # verify that all images have been copied
-copied <- dir_ls(img_dir, recurse=TRUE, type="file")
+copied <- system2("find", args=str_c(img_dir, " -type f"), stdout=TRUE)
 length(copied) == nrow(o)
+
+# remove images that should not be there
+extra_imgs <- copied[!copied %in% o$dest]
+if (length(extra_imgs) > 0 ) {
+  unlink(extra_imgs)
+}
 
 # TODO reformat all images to cut the 31 px at the bottom and keep only the largest object?
 # TODO measure the new images with scikit image?
