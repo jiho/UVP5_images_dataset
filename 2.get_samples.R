c008d1063eb8beaabe4d0b424825660c06a6eff4
Jean-Olivier Irisson <irisson@normalesup.org>
2022-03-05 14:54:52 +0000

    Make it work with separated Ecotaxa/EcoPart
    + clean up a bit and use the new selection of samples by Rainer.

--- 2.get_samples.R
+++ 2.get_samples.R
@@ -1,53 +1,63 @@
 #
-# Fetch almost fully validated UVP5 casts and their metadata
+# Fetch (almost) fully validated UVP5 samples and their metadata
 #
 # (c) 2020 Jean-Olivier Irisson, GNU General Public License v3
 
-# NB: some bits are long, run as a job
+# NB: some bits are long, run as a job in RStudio
 
 source("0.setup.R")
 
 ## Compute candidate samples status ----
 
 # read selected projects
-# projects <- read_csv("https://docs.google.com/spreadsheets/d/1J3ldlk5tU2rgb7aav61FsNXOc8DbRPzankByfVr0Xj0/export?format=csv")
-projects <- read_csv("data/UVP5_projects_selected.csv")
+# (from Google Sheet or downloaded file)
+# TODO finalise the selection of samples, export and read from the exported file, to stabilise the selection
+projects <- read_csv("https://docs.google.com/spreadsheets/d/1CrR-5PdhQ09JSTU482HOkTjtCvXRmpngYDuYSwaGQAo/export?format=csv", col_types=cols())
+# projects <- read_csv("data/UVP5_projects_selected.csv")
 selected_projects <- projects %>% filter(use != "")
 
-# get sorting status of corresponding samples
-samples <- tbl(db, "part_samples") %>%
+# get all samples from those projects
+samples <- tbl(dbp, "part_samples") %>%
   # get ecotaxa sampleids from selected particle projects
-  filter(pprojid %in% !!selected_projects$pprojid) %>%
-  select(psampleid, sampleid, profile=profileid) %>%
-  # get classification status of objects in those
-  left_join(tbl(db, "objects") %>% select(sampleid, classif_qual), by="sampleid") %>%
-  count(profile, sampleid, classif_qual) %>%
+  filter(pprojid %in% local(selected_projects$pprojid)) %>%
+  select(psampleid, sampleid, profile_name=profileid) %>%
+  collect()
+
+# get sorting stats of those samples
+samples_classif <- tbl(dbt, "objects") %>%
+  # limit to samples of potential interest
+  filter(sampleid %in% local(samples$sampleid)) %>%
+  # get classification status of all objects in those
+  count(sampleid, classif_qual) %>%
   ungroup() %>% collect()
 # NB: this takes ~5 min
 
 # compute sorting percentage for those
-samples_validation_status <- samples %>%
+samples_classif_stats <- samples_classif %>%
   # compute sorting percentage
   pivot_wider(names_from=classif_qual, values_from=n, values_fill=0) %>%
   rename(N=`NA`) %>%
   mutate(
     total=V+D+P+N,
     percent_validated=V/total*100
-  )
+  ) %>%
+  # add the psampleid and profile_name
+  left_join(samples)
 
 
 ## Extract a selection of samples ----
 
 # keep only samples almost fully sorted
-selected_samples <- samples_validation_status %>%
+selected_samples <- samples_classif_stats %>%
   filter(percent_validated > 99)
+nrow(selected_samples)
 
 # extract sample level information (from EcoPart because this is the most reliable source)
-selected_samples_info <- tbl(db, "part_samples") %>%
-  filter(sampleid %in% !!selected_samples$sampleid) %>%
+selected_samples_info <- tbl(dbp, "part_samples") %>%
+  filter(sampleid %in% local(selected_samples$sampleid)) %>%
   select(
     # identifiers
-    pprojid, psampleid, sampleid, profile=profileid,
+    pprojid, psampleid, sampleid, profile_name=profileid,
     # location
     lat=latitude, lon=longitude, datetime=sampledate,
     # relevant acquisition metadata
@@ -56,7 +66,7 @@ selected_samples_info <- tbl(db, "part_samples") %>%
     comment
   ) %>%
   # compute depth offset from project level default and sample level one
-  left_join(tbl(db, "part_projects") %>% select(pprojid, default_depthoffset)) %>%
+  left_join(tbl(dbp, "part_projects") %>% select(pprojid, default_depthoffset)) %>%
   mutate(depth_offset=ifelse(is.na(acq_depthoffset), default_depthoffset, acq_depthoffset)) %>%
   select(-acq_depthoffset, -default_depthoffset) %>%
   collect() %>%
@@ -72,22 +82,23 @@ write_tsv(selected_samples_info, "data/UVP5_samples_selected.tsv")
 
 # read the samples used by Thelma
 thelma_samples <- read_csv("data/list_profiles_ecopart-thelma.csv", col_types=cols()) %>%
-  select(title=ecotaxa, ptitle=ecopart, profile=profiles) %>%
-  separate_rows(profile, sep=",")
+  select(title=ecotaxa, ptitle=ecopart, profile_name=profiles) %>%
+  separate_rows(profile_name, sep=",")
 
 # get our current selection of samples
 current_samples <- selected_samples_info %>%
-  select(ends_with("id"), profile) %>%
+  select(ends_with("id"), profile_name) %>%
   # add projects titles
   left_join(select(projects, pprojid, projid, ptitle, title), by=c("pprojid", "projid"))
 
 # profiles in Thelma's selection but not in ours
-missing_in_current <- filter(thelma_samples, ! profile %in% current_samples$profile)
+missing_in_current <- filter(thelma_samples, ! profile_name %in% current_samples$profile_name)
 
+# inspect which and why
 missing_in_current %>%
-  left_join(samples_validation_status) %>%
-  select(title, sampleid, profile, percent_validated) %>%
-  arrange(title, profile)
+  left_join(samples_classif_stats) %>%
+  select(title, sampleid, profile_name, percent_validated) %>%
+  arrange(title, profile_name)
 # -> four samples (an1304_l2_002, 4, 5, 6) are missing in EcoPart now...
 #    uvp5_sn009_2015_p16n now has no images in it
 #    c_msm22_087 has lots of dubious
@@ -96,7 +107,7 @@ missing_in_current %>%
 # missing_in_current %>% group_by(title) %>% summarise(samples=str_c(profile, collapse=",")) %>% write_tsv("to_finish_sorting.tsv")
 
 # profiles in our selection but not in Thelma's
-extra_in_current <- filter(current_samples, ! profile %in% thelma_samples$profile)
+extra_in_current <- filter(current_samples, ! profile_name %in% thelma_samples$profile_name)
 
 # check the project names to make sure we do not have extra projects
 # NB: this has to be done by eye, since some of the project names have changed...
@@ -105,9 +116,10 @@ unique(thelma_samples$title) %>% sort()
 # -> nope, that seems OK
 
 extra_in_current %>%
-  left_join(samples_validation_status) %>%
-  select(title, sampleid, profile, percent_validated) %>%
-  arrange(title, profile)
-# -> 79 extra profiles
+  left_join(samples_classif_stats) %>%
+  select(title, sampleid, profile_name, percent_validated) %>%
+  arrange(title, profile_name) %>%
+  View()
+# -> 444 extra profiles
 #    some are probably due to the 99% instead of 100% validated criterion
-#    others probably have been sorted since
+#    others have been added by Laetitia and Rainer
