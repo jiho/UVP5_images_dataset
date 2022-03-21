5e1876a56ef77614325210c9db844b208823dfb6
juliecoust <jcoustenoble@marie.obs-vlfr.fr>
2022-03-21 18:39:46 +0000

    WIP : contact projects owners data

---
+++ export_contact.py
@@ -0,0 +1,78 @@
+import pandas as pd
+import os
+
+### GET ALL DATA
+
+path_backup_all = "/home/jiho/datasets/UVP5_images_dataset/all_feb13.feather" #TODO UPDATE
+all=pd.read_feather(path_backup_all, columns=["projid", "taxon", "objid"])
+#print(all.columns)
+
+### DF TAXON : projid ; taxon_to_sort ; nb_obj
+#select t0N and othertocheck classified taxons
+df_taxon = all.loc[all['taxon'].str.startswith('t0') | all['taxon'].str.startswith("othertocheck")] 
+#select needed col
+df_taxon = df_taxon[["projid", "taxon", "objid"]]
+#get uniques
+df_taxon = df_taxon.drop_duplicates()
+#get the count of eatch unwanted taxon for eatch projects
+df_taxon = df_taxon.groupby(["projid", "taxon"])["objid"].count().reset_index(name="nb_obj")
+# sort
+df_taxon = df_taxon.sort_values(by=['projid', 'taxon', "nb_obj"], ascending=[True, True, False])
+#print(df_taxon)
+#print(len(df_taxon.projid.unique()))
+
+
+### DF OWNER : projid ; proj_owner_id ; proj_owner_name ; proj_owner_email
+#select needed col
+df_owner = pd.DataFrame({'projid': all["projid"].values})
+#get uniques
+df_owner = df_owner.drop_duplicates()
+# sort
+df_owner = df_owner.sort_values(by='projid')
+#get "Data owner" of eatch projects from google sheet
+projects_info = pd.read_csv("https://docs.google.com/spreadsheets/d/1CrR-5PdhQ09JSTU482HOkTjtCvXRmpngYDuYSwaGQAo/export?format=csv")
+projects_info = projects_info[projects_info["use"]=="x"][["projid","data_owner"]].astype({'projid':'int'})
+df_owner["data_owner"] = [projects_info[projects_info["projid"]==projid]["data_owner"].values[0]for projid in df_owner["projid"]]
+#print(df_owner)
+#print(len(df_owner.projid.unique()))
+
+
+### DF ANNOTATORS : projid ; classif_histo_user_id ; classif_histo_name ; classif_histo_email ; nb_classif
+#select needed col 
+df_projects_ids = pd.DataFrame({'projid': all["projid"].values})
+#get uniques
+df_projects_ids = df_projects_ids.drop_duplicates()
+# sort
+df_projects_ids = df_projects_ids.sort_values(by='projid')
+#print(df_projects_ids)
+
+path = "/home/jcoustenoble/datasets/UVP5_images_dataset/projects"
+#read feathers
+tmp_feathers = []
+for root, dirs, files in os.walk(path):
+    for filename in files:
+        #read and select needed col
+        tmp_feathers.append(pd.read_feather(path+'/'+filename, columns=['projid', 'userid', 'email', 'name', 'n']))
+df_tmp_feathers = pd.concat(tmp_feathers)
+#print(df_tmp_feathers)
+
+#get anotators with more than 1000 contributions on the project
+df_annotators = df_tmp_feathers[df_tmp_feathers.n>=1000]
+#get uniques
+df_annotators = df_annotators.drop_duplicates()
+# drop projects that are not in the final dataset
+df_annotators = df_annotators[df_annotators['projid'].isin(df_projects_ids["projid"].values)]
+# sort
+df_annotators = df_annotators.sort_values(by=['projid', "n"], ascending=[True, False])
+#Rename collums to match the desiered output
+df_annotators.rename(columns={'userid': 'annotator_histo_userid', 'email': 'annotator_histo_email', 'name': 'annotator_histo_name', 'n': 'annotator_histo_n'}, inplace=True)
+#print(df_annotators)
+#print(len(df_annotators["projid"]))
+
+
+### Concat all df
+df_final = pd.merge(left=df_owner,right=df_taxon, left_on='projid', right_on='projid')
+df_final = pd.merge(left=df_final,right=df_annotators, left_on='projid', right_on='projid')
+
+### Save df
+df_final.to_csv("~/datasets/export_contact.csv", index = False, header=True)
---
+++ view_contact.py
@@ -0,0 +1,23 @@
+import pandas as pd
+
+df_final = pd.read_csv("~/datasets/export_contact.csv")
+
+grouped_df = df_final.groupby(by = ["data_owner"])
+
+for key, item in grouped_df:
+    #print(grouped_df.get_group(key), "\n\n")
+    print("DATA OWNER CONTACT : ",key)
+    print("projects : ", grouped_df.get_group(key)["projid"].unique())
+    for projid in grouped_df.get_group(key)["projid"].unique() :
+        tmp = grouped_df.get_group(key).loc[grouped_df.get_group(key)["projid"]==projid]
+        print("------- ", projid, " : ")
+        print("Taxon to reclassify : ")
+        for taxon in tmp.taxon.unique() :
+            print("   ",taxon, " : ", tmp.loc[tmp["taxon"]==taxon].nb_obj.unique()[0], " objects")
+            
+        print("Annotators history : ")
+        for annotator in tmp.annotator_histo_userid.unique() :
+            print("   ", tmp.loc[tmp["annotator_histo_userid"]==annotator].annotator_histo_name.unique()[0], " : ", tmp.loc[tmp["annotator_histo_userid"]==annotator].annotator_histo_n.unique()[0], " objects")
+        
+        print("\n")
+    print("\n\n")
