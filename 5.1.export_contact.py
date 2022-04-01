7812e423e0946b2aac45fecdf29aa0a05724f511
juliecoust <jcoustenoble@marie.obs-vlfr.fr>
2022-04-01 13:23:01 +0000

    contact data owners V2

--- 5.1.export_contact.py
+++ 5.1.export_contact.py
@@ -1,13 +1,15 @@
 import pandas as pd
 import os
 
-### UTIL
+## UTIL
 path_all_feather = os.path.expanduser("~/datasets/UVP5_images_dataset/all.feather")
 path_dataset_with_annotators = os.path.expanduser("~/datasets/UVP5_images_dataset_with_annotators/projects")
+csvPath_reclassify_taxa = os.path.expanduser("~/datasets/export_contact_reclassify_taxa.csv")
+csvPath_annotators_credits = os.path.expanduser("~/datasets/export_contact_annotators_credits.csv")
 
-### GET ALL DATA
-all=pd.read_feather(path_all_feather, columns=["projid", "taxon", "objid"])
-print(all.columns)
+## GET ALL DATA
+# read all.feather
+all=pd.read_feather(path_all_feather, columns=["projid", "taxon", "objid","annotator"])
 
 # read projid.feathers
 tmp_projid_feathers = []
@@ -16,23 +18,28 @@ for root, dirs, files in os.walk(path_dataset_with_annotators):
         # read and select needed col
         tmp_projid_feathers.append(pd.read_feather(path_dataset_with_annotators+'/'+filename, columns=['projid', 'userid', 'email', 'name', 'n']))
 projid_feathers = pd.concat(tmp_projid_feathers)
-print(projid_feathers.columns)
 
-### DF TAXON : projid ; taxon_to_sort ; nb_obj
-# select t0N and othertocheck classified taxa
+## RECLASSIFY TAXA DF Owner_df & Taxa_df  (current annotators)
+### DF TAXA : projid ; taxon_to_sort ; nb_obj ; bad_taxa_annotators
+
+# select t0N and othertocheck classified taxons
 df_taxon = all.loc[all['taxon'].str.startswith('t0') | all['taxon'].str.startswith("othertocheck")] 
-# select needed col
-df_taxon = df_taxon[["projid", "taxon", "objid"]]
-# get uniques
-df_taxon = df_taxon.drop_duplicates()
-# get the count of eatch unwanted taxon for eatch projects
-df_taxon = df_taxon.groupby(["projid", "taxon"])["objid"].count().reset_index(name="nb_obj")
+# get the list of annotators that set bad taxa 
+bad_taxa_annotators=df_taxon[["projid", "annotator", "objid"]].drop_duplicates()
+# get the count of eatch unwanted taxon annotation for eatch annotator
+bad_taxa_annotators = bad_taxa_annotators.groupby(["projid", "annotator"])["objid"].count().reset_index(name="nb_bad_obj_annoted")
 # sort
-df_taxon = df_taxon.sort_values(by=['projid', 'taxon', "nb_obj"], ascending=[True, True, False])
-# print(df_taxon)
-# print(len(df_taxon.projid.unique()))
+bad_taxa_annotators = bad_taxa_annotators.sort_values(by=['projid', 'nb_bad_obj_annoted', 'annotator'], ascending=[True, False, True])
+#get others needed col
+df_taxon = df_taxon[["projid", "taxon", "objid"]].drop_duplicates()
+# get the count of eatch unwanted taxon for eatch projects
+df_taxon = df_taxon.groupby(["projid", "taxon"])["objid"].count().reset_index(name="nb_bad_obj")
+# merge bad_taxa_annotators into df_taxon on projid
+df_taxon = pd.merge(df_taxon, bad_taxa_annotators, on='projid', how="outer")
+# sort
+df_taxon = df_taxon.sort_values(by=['projid', 'taxon', "nb_bad_obj"], ascending=[True, True, False])
 
-### DF OWNER : proj_owner_email
+### DF OWNER : proj_id, proj_title, proj_owner_email
 # select needed col
 df_owner = pd.DataFrame({'projid': all["projid"].values})
 # get uniques
@@ -43,35 +50,56 @@ df_owner = df_owner.sort_values(by='projid')
 projects_info = pd.read_csv("https://docs.google.com/spreadsheets/d/1CrR-5PdhQ09JSTU482HOkTjtCvXRmpngYDuYSwaGQAo/export?format=csv")
 projects_info = projects_info.dropna(subset=['use'])[["projid", "title", "data_owner"]].astype({'projid':'int'})
 df_owner["title"] = [projects_info[projects_info["projid"]==projid]["title"].values[0]for projid in df_owner["projid"]]
+df_owner["url"] = ["https://ecotaxa.obs-vlfr.fr/prj/"+str(projid) for projid in df_owner["projid"]]
 df_owner["data_owner"] = [projects_info[projects_info["projid"]==projid]["data_owner"].values[0]for projid in df_owner["projid"]]
-# print(df_owner)
-# print(len(df_owner.projid.unique()))
+
+### Merge df_owner, df_taxon to create RECLASSIFY TAXA DF
+df_reclassify_taxa = pd.merge(df_owner, df_taxon, on='projid', how="outer")
+# Save as csv
+df_reclassify_taxa.to_csv(csvPath_reclassify_taxa, index = False, header=True)
 
 
-### DF ANNOTATORS : projid ; classif_histo_user_id ; classif_histo_name ; classif_histo_email ; nb_classif
-# select needed col 
-df_projects_ids = pd.DataFrame({'projid': all["projid"].values})
+## ANNOTATORS CREDITS DF owner & annotator (current + histo)
+### DF ANNOTATORS : projid ; classif_user_id ; classif_name ; classif_email ; nb_classif (current + histo)
+
+#### CURRENT
+# get the list of annotators that set current taxa 
+current_taxa_annotators=all[["projid", "annotator", "objid"]].drop_duplicates()
+# get the count of current classified objects for eatch annotator
+current_taxa_annotators = current_taxa_annotators.groupby(["projid", "annotator"])["objid"].count().reset_index(name="nb_obj_annoted")
+
+#### HISTO
 # get uniques
-df_projects_ids = df_projects_ids.drop_duplicates()
-# sort
-df_projects_ids = df_projects_ids.sort_values(by='projid')
-# print(df_projects_ids)
-
-# get uniques anotators with more than 1000 contributions on the project
-df_annotators = projid_feathers[projid_feathers.n>=1000].drop_duplicates()
+histo_taxa_annotators = projid_feathers.drop_duplicates()
 # drop projects that are not in the final dataset
-df_annotators = df_annotators[df_annotators['projid'].isin(df_projects_ids["projid"].values)]
+histo_taxa_annotators = histo_taxa_annotators[histo_taxa_annotators['projid'].isin(current_taxa_annotators["projid"].unique())]
 # sort
-df_annotators = df_annotators.sort_values(by=['projid', "n"], ascending=[True, False])
+histo_taxa_annotators = histo_taxa_annotators.sort_values(by=['projid', "n"], ascending=[True, False])
 # Rename collums to match the desiered output
-df_annotators.rename(columns={'userid': 'annotator_histo_userid', 'email': 'annotator_histo_email', 'name': 'annotator_histo_name', 'n': 'annotator_histo_n'}, inplace=True)
-# print(df_annotators)
-# print(len(df_annotators["projid"]))
+histo_taxa_annotators.rename(columns={'userid': 'annotator_histo_userid', 'email': 'annotator_histo_email', 'name': 'annotator_histo_name', 'n': 'annotator_histo_n'}, inplace=True)
+
+#### MERGE AND FILTER
+# for eatch project and annotators add current and histo count of annotations 
+# on "annotator"(email) and "annotator_histo_email"(email
+df_annotators_tmp = pd.merge(current_taxa_annotators, histo_taxa_annotators, left_on=['projid', 'annotator'], right_on=['projid', 'annotator_histo_email'], how="outer")
+# fill missing nb with 0
+df_annotators_tmp[['nb_obj_annoted','annotator_histo_n']] = df_annotators_tmp[['nb_obj_annoted','annotator_histo_n']].fillna(0)
+# fill missing emails
+df_annotators_tmp['annotator_histo_email'] = df_annotators_tmp['annotator_histo_email'].fillna(df_annotators_tmp['annotator_histo_n'])
+# select only usfull col
+df_annotators= df_annotators_tmp[["projid", "annotator_histo_email", "annotator_histo_name"]]
+# sum the total of classif 
+df_annotators["tot_annotations"] = df_annotators_tmp["annotator_histo_n"] + df_annotators_tmp["nb_obj_annoted"]
+df_annotators["tot_annotations"] = df_annotators["tot_annotations"].astype(int)
+# rename col
+df_annotators.rename(columns={'annotator_histo_email': 'annotator_email', 'annotator_histo_name': 'annotator_name'}, inplace=True)
+# filter n>1000
+df_annotators = df_annotators[df_annotators.tot_annotations>=1000].drop_duplicates()
 
 
-### Concat all df
-df_final = pd.merge(df_owner, df_taxon, on='projid', how="outer")
-df_final = pd.merge(df_final, df_annotators, on='projid', how="outer")
-
-### Save df
-df_final.to_csv("~/datasets/export_contact.csv", index = False, header=True)
+### Merge df_owner, df_annotators to create ANNOTATORS CREDITS DF 
+df_annotators_credits = pd.merge(df_owner, df_annotators, on='projid', how="outer")
+# sort
+df_annotators_credits = df_annotators_credits.sort_values(by=['projid', 'tot_annotations'], ascending=[True, False])
+# Save as csv
+df_annotators_credits.to_csv(csvPath_annotators_credits, index = False, header=True)
--- 5.2.view_contact.py
+++ 5.2.view_contact.py
@@ -1,38 +1,121 @@
 import pandas as pd
 import os
+#google sheet api
+from auth import spreadsheet_service
+from auth import drive_service
 
 ### UTIL
-filePath = os.path.expanduser("~/datasets/datasets/view_contact.txt")
-csvPath = os.path.expanduser("~/datasets/export_contact.csv")
 
+filePath = os.path.expanduser("~/datasets/view_contact.txt")
+csvPath_reclassify_taxa = os.path.expanduser("~/datasets/export_contact_reclassify_taxa.csv")
+csvPath_annotators_credits = os.path.expanduser("~/datasets/export_contact_annotators_credits.csv")
+
+def new_google_sheet(data):
+    return "#sheetURL", "#sheetId"
+    # Create sheet
+    spreadsheet_details = {
+    'properties': {
+        'title': 'Dataset UVP5 credits for ' + data['data_owner'].unique()[0]
+        }
+    }
+    sheet = spreadsheet_service.spreadsheets().create(body=spreadsheet_details).execute()
+    sheetId = sheet.get('spreadsheetId')
+    sheetURL = sheet.get('spreadsheetUrl')
+    
+    # Grant permissions
+    users = ['julie.coustenoble@imev-mer.fr']#, 'irisson@normalesup.org', 'lars.stemmann@imev-mer.fr', data['data_owner'].unique()[0]]
+    for user_email in users :
+        permission1 = {
+            'type': 'user',
+            'role': 'writer',
+            'emailAddress': user_email
+        }
+        sheets_file = drive_service.permissions().create(fileId=sheetId, body=permission1).execute()
+    
+    # Set col names
+    values = [["Project name", "Project URL", "Data owner contact", "Annotators", "Additional authors"]]
+    body = {'values': values}
+    result = spreadsheet_service.spreadsheets().values().append(
+        spreadsheetId=sheetId, range="Sheet1!A1:E1",
+        valueInputOption='USER_ENTERED', body=body).execute()
+    
+    return sheetURL, sheetId
+  
+def fill_google_sheet(project_data, sheetId, i):
+    return #TODO remove 
+    # Add project_data to sheetId at line i
+    project_data=project_data.astype({"tot_annotations": str})
+    values = [[
+        project_data["title"].unique()[0], 
+        project_data["url"].unique()[0], 
+        project_data["data_owner"].unique()[0], 
+        ', '.join(project_data["annotator_name"].unique()+" (n = " +project_data["tot_annotations"].unique()+")"), ""
+    ]]
+    body = {'values': values}
+    result = spreadsheet_service.spreadsheets().values().append(
+        spreadsheetId=sheetId, range="Sheet1!A"+str(i)+":E"+str(i),
+        valueInputOption='USER_ENTERED', body=body).execute()
+
+# reset file if exist
 try:
     os.remove(filePath)
 except:
     pass
 
-df_final = pd.read_csv(csvPath)
-grouped_df = df_final.groupby(by = ["data_owner"])
+# get stored data
+df_reclassify_taxa = pd.read_csv(csvPath_reclassify_taxa)
+df_annotators_credits = pd.read_csv(csvPath_annotators_credits)
 
+# group by data owner
+grouped_df = df_reclassify_taxa.groupby(by = ["data_owner"])
+
+# open the file and starts writing in it trought prints
 with open(filePath, "a") as f:
-  for key, item in grouped_df:
-      #print(grouped_df.get_group(key), "\n\n")
-      print("DATA OWNER CONTACT : ", key, file=f)
-      print("projects : ", grouped_df.get_group(key)["projid"].unique(), file=f)
-      for projid in grouped_df.get_group(key)["projid"].unique() :
-          tmp = grouped_df.get_group(key).loc[grouped_df.get_group(key)["projid"]==projid]
-          print("------- ", projid, " : ", file=f)
-          print("Taxon to reclassify : ", file=f)
-          for taxon in tmp.taxon.unique() :
-            try :
-                print("   ",taxon, " : ", tmp.loc[tmp["taxon"]==taxon].nb_obj.unique()[0], " objects", file=f)
-            except : 
-                print("   ", "Nothing to reclassify", file=f)             
-          print("Annotators history : ", file=f)
-          for annotator in tmp.annotator_histo_userid.unique() :
-            try :
-                print("   ", tmp.loc[tmp["annotator_histo_userid"]==annotator].annotator_histo_name.unique()[0], " : ", tmp.loc[tmp["annotator_histo_userid"]==annotator].annotator_histo_n.unique()[0], " annotations", file=f)
-            except : 
-                print("   ", "No significant annotator", file=f)
-          print("\n", file=f)
-      
-  f.close()   
+    for key, item in grouped_df:
+        # create the google sheet permissions and headers
+        sheetUrl, sheetId = new_google_sheet(df_annotators_credits.loc[df_annotators_credits["data_owner"]==key])    
+        # cpt for row in exported google sheetstart at 2
+        i = 2
+
+        # general intro
+        print("Destinataire : ", key, file=f)
+        print('Sujet : "Effort to homogenise and publish UVP5 images datasets" \n', file=f)
+        print("#TODO corps du message, le contexte du projet etc.  \n", file=f)
+        
+        # list projects for this owner
+        print("You are identified as the data owner of EcoPart projects linked to the following EcoTaxa projects : ", file=f)
+        for projid in grouped_df.get_group(key)["projid"].unique() :
+            tmp = grouped_df.get_group(key).loc[grouped_df.get_group(key)["projid"]==projid]
+            x="#TODO"
+            print(tmp.url.unique()[0], " : ", tmp.title.unique()[0], file=f)
+          
+        # list undefined or temporary categories
+        print("\nWe noticed that some images are still in undefined or temporary categories. Those represent", x," of the living objects in your projects. It would be really great if you could create new categories with descriptive names (e.g. 'long with dark center', 'round with fuzzy edge') branched within the taxonomy of life (or check if such names already exist) and move the images there. It would allow other UVP users to follow this nomenclature and would avoid loosing the data when we aggregate it (because your t001 is not the same as that of others). We can assist you with this process.", file=f)
+        print("For the categories that still need sorting (othertocheck, some temporary ones), it would be excellent if you could make a last bit of effort to sort what can be sorted. Again, we may be able to assist you if need be.", file=f)
+        print("Here is the list of the undefined or temporary ones :", file=f)
+        for projid in grouped_df.get_group(key)["projid"].unique() :
+            tmp = grouped_df.get_group(key).loc[grouped_df.get_group(key)["projid"]==projid]
+            #fill the  google sheet with project data
+            fill_google_sheet(df_annotators_credits.loc[df_annotators_credits["projid"]==projid], sheetId, i)
+            i= i+1
+            print("\n",tmp.url.unique()[0], " : ", file=f)
+            for taxon in tmp.taxon.unique() :
+                try :
+                    print("   ",taxon, " : ", tmp.loc[tmp["taxon"]==taxon].nb_bad_obj.unique()[0], " objects", file=f)
+                except : 
+                    print("   ", "Nothing to reclassify", file=f)
+            #print("   ","Latest annotators in these taxa : ", file=f)
+            #for annotator in tmp.annotator.unique() :
+            #    try :
+            #        print("      ", annotator, " : ", tmp.loc[tmp["annotator"]==annotator].nb_bad_obj_annoted.unique()[0], "objects", file=f)
+            #    except : 
+            #        print("   ", "ERROR", file=f)
+
+        print("""
+    Finally, to compile a fair list of authors, we collected the names of all people who provided more that 1000 annotations in this Google Sheet : """, sheetUrl, """.\nCould you please edit it to:
+    - remove names of people you feel should not be authors (e.g. employees/students who have since left the field and have contributed little)
+    - add the names and affiliations of people you would like to see added (e.g. participants to the cruises during which the data was collected, people having sorted the images prior to their import on EcoTaxa)
+
+    Thank you in advance!""", file=f)
+        print("\n\n------------------------------------------------------------- \n\n", file=f)
+    f.close()   
