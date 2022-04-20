2f088d74ea0dc00f0ff47c4ea262433e17edc7ca
juliecoust <jcoustenoble@marie.obs-vlfr.fr>
2022-04-20 08:22:53 +0000

    update email content

--- 5.2.view_contact.py
+++ 5.2.view_contact.py
@@ -73,7 +73,9 @@ with open(filePath, "a") as f:
       print("""Sujet : "Effort to homogenise and publish UVP5 images datasets"
 
 Dear Colleague,
-  
+
+Following the effort for the UVP particle size distribution dataset and paper (https://essd.copernicus.org/preprints/essd-2022-51/) we aim at publishing a dataset and paper focussed on UVP5 images, validated taxonomically in EcoTaxa. 
+
 You are identified as the data owner of EcoPart projects linked to the following EcoTaxa projects : """, file=f)
       
       sheetUrl, sheetId = new_google_sheet(df_annotators_credits.loc[df_annotators_credits["data_owner"]==key])    
@@ -84,18 +86,15 @@ You are identified as the data owner of EcoPart projects linked to the following
       
       x_tmp=item[["to_reclassify_nb_objects","living_nb_objects", "projid"]].drop_duplicates()
       x="{:.2f}".format((100*x_tmp["to_reclassify_nb_objects"].sum())/x_tmp["living_nb_objects"].sum())
-      print("""
-We are contacting you with respect to the publication of a global UVP5 image dataset and an accompanying data paper in EESD. 
-We would like to invite you to co-author the dataset and data paper, as you were part of this endeavor (maybe without knowing it explicitly until now  and pushed the consistent use of the UVP5 during several cruises, contributed data or contributed to the data aggregation and quality control work needed to get the dataset together. 
-  
-You might have gotten notice of this action already and probably was involved in the UVP ‘particle’ data paper by Rainer Kiko. We are doing a similar effort but for the images stored and validated on ECOTAXA. We are now contacting data owners that have validated their images. 
-This dataset has to be seen as the current state of validation (~2800 profiles) that is frozen for the purpose of this paper and in the future, better sorting and more profiles will allow the building up of the database (currently up to 9000 profiles) that could be saved as a second version.
-  
-The goal is to make public all the ecotaxa data (images, metadata, morphological measures, identification) of the UVP5sd and UVP5hd for other users. The data is including all categories for which validation of plankton has been made at 99%. Plankton must be sorted in homogeneous categories (which is the case thanks to Ecotaxa) and the metadata must be  corrected (which is the case thanks to the effort by Rainer in the previous paper).
-The data product will contain the details of the taxonomic level currently in ECOTAXA but the paper will describe aggregated groups (for example at copepod level even if detailed recognition exist). The data product will also contains all the vignettes of the detritus but as only one category because the sorting is not homogeneous among us.
-  
-We noticed that some images are still in undefined or temporary categories. Those represent""", str(x),"""% of the living objects in your projects. It would be really great if you could create new categories with descriptive names (e.g. 'long with dark center', 'round with fuzzy edge') branched within the taxonomy of life (or check if such names already exist) and move the images there. It would allow other UVP users to follow this nomenclature and would avoid loosing the data when we aggregate it (because your t001 is not the same as that of others). We can assist you with this process.
+      print("""and we would like to invite you as a co-author.
+
+You may have heard about this effort over the past years and contributed to the homogenisation of taxonomic sorting; we are now trying to finalise it. We are currently focused on ~3800 profiles for which sorting is well advanced and consistent. It will surely be expanded in future versions.
+
+This dataset will be published as an archive of files, frozen and independent from EcoTaxa, available for users to download. It will contain the details of the taxonomic sorting currently in EcoTaxa (including for the detritus) but also data aggregated at the level of ~30 coarse categories for which we determined the sorting to be consistent. 
+
+Still, in this last effort, we noticed that some plankton images are still in undefined or temporary categories; although some of these organisms may be living, they will have to be discarded in the aggregation (because your 't001' is not the same as that of others). They represent """, str(x),"""% of the living objects in your projects. It would be great if you could create new categories with descriptive names (e.g. 'long with dark center', 'round with fuzzy edge') branched within the taxonomy of life (or check if such names already exist) and move the images there. It would allow other UVP users to follow this nomenclature and would avoid losing the data when aggregated . We can assist you with this process.
 For the categories that still need sorting (othertocheck, some temporary ones), it would be excellent if you could make a last bit of effort to sort what can be sorted. Again, we may be able to assist you if need be.
+
 Here is the list of the undefined or temporary ones  :""", file=f)
       
       # initialize / empty projects_data
@@ -114,18 +113,18 @@ Here is the list of the undefined or temporary ones  :""", file=f)
       #fill the  google sheet with project data
       fill_google_sheet(projects_data, sheetId)
       print("""
+The proposed changes are not required and the dataset, as it is today, can be published. The additional sorting is for the odd categories that we could decide to include or not in the dataset if they turn out to be numerous/relevant.  
+
+Can you inform us if you are willing to participate in this publication effort and if you are willing to engage in some additional sorting?
+
+I will soon share a draft with the current version of the paper; the goal is to have it submitted by the end of june.
+
 Finally, to compile a fair list of authors, we collected the names of all people who provided more that 1000 annotations in this Google Sheet : """, sheetUrl, """.\nCould you please edit it to:
-  - remove names of people you feel should not be authors (e.g. employees/students who have since left the field and have contributed little)
-  - add the names and affiliations of people you would like to see added (e.g. participants to the cruises during which the data was collected, people having sorted the images prior to their import on EcoTaxa)
-  
-Can you inform us if you are willing to participate to this effort.
-The dataset preparation is already in a good state (waiting for your changes) and I will soon share a draft with the current version.
-The goal is to have a draft submitted for the end of june.
-  
-  
+- remove names of people you feel should not be authors (e.g. employees/students who have since left the field and have contributed little) 
+- add the names and affiliations of people you would like to see added (e.g. participants to the cruises during which the data was collected, people having sorted the images prior to their import on EcoTaxa)
+
+
 Thank you in advance!
-  
-  
-Lars Stemmann""", file=f)
+""", file=f)
       print("\n\n------------------------------------------------------------- \n\n", file=f)
     f.close()   
