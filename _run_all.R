f0c5073ce31eb6a8026c49400f8f3c28e4ae3f3d
Jean-Olivier Irisson <irisson@normalesup.org>
2024-01-01 23:08:04 +0000

    Run 5 first steps

--- _run_all.R
+++ _run_all.R
@@ -3,7 +3,9 @@
 #
 # (c) 2023 Jean-Olivier Irisson, GNU General Public License v3
 
-# 2023-12-29 14:00
+# 2024-01-02 00:07
 source("1.list_projects.R")
 source("2.get_samples.R")
 source("3.get_volumes.R")
+source("4.get_objects.R")
+source("5.reformat_objects.R")
