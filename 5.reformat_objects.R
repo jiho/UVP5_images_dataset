e99f465a306ea803db08b03bb8ac7b015b456755
Jean-Olivier Irisson <irisson@normalesup.org>
2024-01-01 12:59:59 +0000

    Review data processing code

--- 5.reformat_objects.R
+++ 5.reformat_objects.R
@@ -4,7 +4,6 @@
 # (c) 2020 Jean-Olivier Irisson, GNU General Public License v3
 
 source("0.setup.R")
-library("data.tree")
 library("arrow")
 library("ecotaxar")
 
@@ -19,42 +18,18 @@ o <- map_dfr(proj_files, read_feather)
 nrow(o)
 # 7,186,461 on 2020-12-16 00:16
 # 7,820,053 on 2021-02-13 00:31 (added dubious objects and some additional profiles)
+# 8,565,863 on 2023-12-29 22:47
 
 
-## Enrich data with extra fields ----
+## Restrict to relevant data -----
 
-# add taxonomic names (unique at the level of the whole dataset)
-taxo <- extract_taxo(dbt, o$classif_id)
-o$taxon <- taxo_name(o$classif_id, taxo, unique=TRUE)
-o$lineage <- lineage(o$classif_id, taxo)
+summary(o$imgrank)
 
-# add user names
-userids <- unique(o$classif_who) %>% as.integer()
-users <- tbl(dbt, "users") %>%
-  select(id, email) %>%
-  filter(id %in% !!userids) %>%
-  collect()
-o$annotator <- users$email[match(o$classif_who, users$id)]
-
-# shift depth by the depth offset
 o <- o %>%
-  left_join(select(samples, sampleid, depth_offset), by="sampleid") %>%
-  mutate(depth=depth+depth_offset)
-
-# compute sizes in human understandable units
-o <- left_join(o, select(samples, sampleid, acq_pixel), by="sampleid") %>%
-  mutate(
-    area_mm2 = area * acq_pixel^2,
-    esd_mm = 2 * sqrt(area_mm2 / pi),
-    vol_mm3 = 4/3 * pi * (esd_mm^3),
-    length_mm = major * acq_pixel
-  )
-
-
-## Restrict to relevant objects -----
-
-nrow(o)
-# 7820053
+  # remove columns that are only in a few projects
+  select(-(rawvig:image_index)) %>%
+  # remove imgrank, we only have 0
+  select(-imgrank)
 
 # keep only objects for which we have a corresponding water volume
 # indeed, when we have no volume the data is useless
@@ -63,12 +38,17 @@ nrow(o)
 # - different handling of particles and large objects (like it was the case at the beginning)
 # - a volume that spiked and was therefore removed at a previous step
 volume <- read_tsv("data/UVP5_volumes.tsv.gz", col_types=cols())
+# shift depth by the depth offset
+o <- o %>%
+  left_join(select(samples, sampleid, depth_offset), by="sampleid") %>%
+  mutate(depth=depth+depth_offset)
+# match with water volume imaged by depth bin
 o_wv <- o %>%
   mutate(mid_depth_bin=floor(depth/5)*5 + 2.5) %>%
   inner_join(volume)
 
 nrow(o) - nrow(o_wv)
-# removed 137,629 objects
+# removed 210,451 objects
 
 # remove objects with tag == 2 which correspond to faulty lights and/or upward movement during the downcast
 o_t <- filter(o_wv, tag != 2)
@@ -93,9 +73,36 @@ o_up <- o_t %>%
   ungroup()
 
 nrow(o_t) - nrow(o_up)
-# removed 88,281 additional rows
+# removed 84,741 additional rows
 
-o <- o_up
+# save the cleaned version
+oc <- o_up
+
+
+## Enrich data with extra fields ----
+
+# add taxonomic names (unique at the level of the whole dataset)
+taxo <- extract_taxo(dbt, oc$classif_id)
+oc$taxon <- taxo_name(oc$classif_id, taxo, unique=TRUE)
+oc$lineage <- lineage(oc$classif_id, taxo)
+
+# identify users through names and emails
+userids <- oc$classif_who_all %>% unlist() %>% unique() %>% as.integer()
+users <- tbl(dbt, "users") %>%
+  select(id, name, email) %>%
+  filter(id %in% !!userids) %>%
+  collect() %>%
+  mutate(user=str_c(str_to_title(name), " <", email, ">"))
+oc$annotators <- map(oc$classif_who_all, function(x) {users$user[match(x, users$id)]})
+
+# compute sizes in human understandable units
+oc <- left_join(oc, select(samples, sampleid, acq_pixel), by="sampleid") %>%
+  mutate(
+    area_mm2 = area * acq_pixel^2,
+    esd_mm = 2 * sqrt(area_mm2 / pi),
+    vol_mm3 = 4/3 * pi * (esd_mm^3),
+    length_mm = major * acq_pixel
+  )
 
 
 ## Prepare taxonomic regrouping ----
@@ -128,17 +135,21 @@ filter(o, str_detect(taxon, "t0")) %>% count(taxon, projid) %>% filter(n>500)
 ## Write data to disk ----
 
 # remove irrelevant variables and reorder columns
-select(o,
-    # identifiers
-    projid, sampleid, objid, origid, file_name,
-    # taxonomy
-    taxon, lineage, annotator, annotation_date=classif_when,
-    # depth
-    depth, mid_depth_bin,
-    # measurements in human readable units
-    contains("_mm"),
-    # zooprocess descriptors
-    area:skeleton_area,
+select(oc,
+       # identifiers
+       projid, sampleid, objid, origid,
+       # image
+       file_name,
+       # taxonomy
+       taxon, lineage,
+       # annotators
+       annotators, last_annotator_id=classif_who, last_annotation_date=classif_when,
+       # depth
+       depth, mid_depth_bin,
+       # measurements in human readable units
+       contains("_mm"),
+       # zooprocess descriptors
+       area:skeleton_area,
   ) %>%
   # add save to disk for now
   write_feather(path=file.path(data_dir, "all.feather"))
