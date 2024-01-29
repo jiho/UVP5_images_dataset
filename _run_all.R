73ca7c97560b5e31f60f9a73cc4322041e09a7ba
Jean-Olivier Irisson <irisson@normalesup.org>
2024-01-29 00:42:20 +0000

    A bit of spacing

--- _run_all.R
+++ _run_all.R
@@ -6,21 +6,30 @@
 # 2024-01-28 00:31
 message("\nList projects")
 source("1.list_projects.R")
+
 message("\nGet samples")
 source("2.get_samples.R")
+
 message("\nGet volumes")
 source("3.get_volumes.R")
+
 message("\nGet objects")
 source("4.get_objects.R")
+
 message("\nReformat objects")
 source("5.reformat_objects.R")
+
 message("\nRegroup taxa")
 source("6.regroup_taxa.R")
+
 message("\nPrepare email for authors")
 source("7.prepare_contact_with_authors.R")
+
 message("\nGet images")
 source("8.get_images.R")
+
 message("\nFinalise dataset")
 source("9.finalise_dataset.R")
+
 message("\nProduce plots")
 source("10.plot.R")
