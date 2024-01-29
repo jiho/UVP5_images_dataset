9ad8c8c5fbf3a09deddf6d17ab0dbad2950c7ba6
Jean-Olivier Irisson <irisson@normalesup.org>
2024-01-29 23:47:34 +0000

    Simplify the running of all steps

--- _run_all.R
+++ _run_all.R
@@ -3,33 +3,29 @@
 #
 # (c) 2023 Jean-Olivier Irisson, GNU General Public License v3
 
+# Last run on:
 # 2024-01-28 00:31
-message("\nList projects")
-source("1.list_projects.R")
 
-message("\nGet samples")
-source("2.get_samples.R")
+run_file <- function(f) {
+  # list environment content
+  vars <- ls(envir=.GlobalEnv)
+  # run code
+  message("\n", basename(f))
+  source(f)
+  # cleanup environment
+  new_vars <- setdiff(ls(envir=.GlobalEnv), vars)
+  rm(list=new_vars, envir=.GlobalEnv)
 
-message("\nGet volumes")
-source("3.get_volumes.R")
+  return(invisible(TRUE))
+}
 
-message("\nGet objects")
-source("4.get_objects.R")
-
-message("\nReformat objects")
-source("5.reformat_objects.R")
-
-message("\nRegroup taxa")
-source("6.regroup_taxa.R")
-
-message("\nPrepare email for authors")
-source("7.prepare_contact_with_authors.R")
-
-message("\nGet images")
-source("8.get_images.R")
-
-message("\nFinalise dataset")
-source("9.finalise_dataset.R")
-
-message("\nProduce plots")
-source("10.plot.R")
+run_file("1.list_projects.R")
+run_file("2.get_samples.R")
+run_file("3.get_volumes.R")
+run_file("4.get_objects.R")
+run_file("5.reformat_objects.R")
+run_file("6.regroup_taxa.R")
+run_file("7.prepare_contact_with_authors.R")
+run_file("8.get_images.R")
+run_file("9.finalise_dataset.R")
+run_file("10.plot.R")
